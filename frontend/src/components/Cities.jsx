891a73645b39ccfceb888c20ab13f6714a36224b
Agustin Fiori <agus_fiori@hotmail.com>
2021-01-22 18:05:31 -0300

    Sprint 2 Final Commit

--- frontend/src/components/Cities.jsx
+++ frontend/src/components/Cities.jsx
@@ -4,24 +4,23 @@ import { useState, useEffect } from "react";
 import { v4 as uuidv4 } from "uuid";
 import { Link } from "react-router-dom";
 import { Loading } from "./Loader.jsx";
-// import { Lines } from "react-preloaders";
 // import axios from 'axios';
 
 const Cities = () => {
   const [cities, setCities] = useState([]);
   const [filteredCities, setFilteredCities] = useState([]);
-  const [loading, setLoading] = useState(false);
-  // const [loading, setLoading] = useState(true);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:4000/cities")
       .then((respuesta) => respuesta.json())
       .then((data) => {
         setCities(data.respuesta);
-        setLoading(true);
+        setLoading(false);
       })
       .catch((err) => {
         console.log("error");
+        setLoading(false);
       });
   }, []);
 
@@ -48,7 +47,7 @@ const Cities = () => {
     setFilteredCities(filtered);
   }
 
-  if (filteredCities.length > 0 && loading === true) {
+  if (loading || filteredCities.length === 0) {
     return (
       <div className="cities">
         <h1>Cities</h1>
@@ -62,69 +61,56 @@ const Cities = () => {
             autoComplete="off"
           />
         </h2>
-        <div className="citiesContainer">
-          {filteredCities.map(({ cityName, cityPic, _id }) => {
-            return (
-              <Link to={`/city/${_id}`} key={uuidv4()}>
-                <div className="card">
-                  <div
-                    className="cityImage"
-                    style={{
-                      backgroundImage: `url(${cityPic})`,
-                    }}
-                  >
-                    <div className="cityName">{cityName}</div>
-                  </div>
-                </div>
-              </Link>
-            );
-          })}
-        </div>
-      </div>
-    );
-  } else if (loading === false) {
-    return (
-      <div className="cities">
-        <h1>Cities</h1>
-        <h2 className="filter">
-          <input
-            type="text"
-            placeholder="Search cities..."
-            name="filter"
-            id="filter"
-            onChange={filter}
-            autoComplete="off"
-          />
-        </h2>
-        <Loading />
-      </div>
-    );
-  } else if (filteredCities.length === 0) {
-    return (
-      <div className="cities">
-        <h1>Cities</h1>
-        <h2 className="filter">
-          <input
-            type="text"
-            placeholder="Search cities..."
-            name="filter"
-            id="filter"
-            onChange={filter}
-            autoComplete="off"
-          />
-        </h2>
-        <div className="noCities">
-          <div className="noCitiesMsg">
-            <h5>
-              Looks like there are no itineraries for the city you're looking
-              for...
-            </h5>
-            <h6>Try another one !</h6>
+        {loading ? (
+          <Loading />
+        ) : (
+          <div className="noCities">
+            <div className="noCitiesMsg">
+              <h5>
+                Looks like there are no itineraries for the city you're looking
+                for...
+              </h5>
+              <h6>Try another one !</h6>
+            </div>
           </div>
-        </div>
+        )}
       </div>
     );
   }
+
+  return (
+    <div className="cities">
+      <h1>Cities</h1>
+      <h2 className="filter">
+        <input
+          type="text"
+          placeholder="Search cities..."
+          name="filter"
+          id="filter"
+          onChange={filter}
+          autoComplete="off"
+        />
+      </h2>
+      <div className="citiesContainer">
+        {filteredCities.map(({ cityName, cityPic, _id }) => {
+          return (
+            <Link to={`/city/${_id}`} key={uuidv4()}>
+              <div className="card">
+                <div
+                  className="cityImage"
+                  style={{
+                    backgroundImage: `url(${cityPic})`,
+                  }}
+                >
+                  <div className="cityName">{cityName}</div>
+                </div>
+              </div>
+            </Link>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Cities;
