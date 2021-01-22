615b526ee043f3612655f7dfda90800253179dd3
Agustin Fiori <agus_fiori@hotmail.com>
2021-01-22 18:37:11 -0300

    agregado .env

--- config/database.js
+++ config/database.js
@@ -2,16 +2,12 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 // conexion a la BASE DE DATOS
-// process.env.MONGODB va a ir como primer parametro de .connect
 mongoose
-  .connect(
-    "mongodb+srv://agusfiori:E77G9IxKukllBUyW@cluster0.2722e.mongodb.net/mytinerary-fiori?retryWrites=true&w=majority",
-    {
-      useNewUrlParser: true,
-      useCreateIndex: true,
-      useUnifiedTopology: true,
-      useFindAndModify: false,
-    }
-  )
+  .connect(process.env.MONGODB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
   .then(() => console.log("Database connected"))
   .catch((error) => console.log(error));
